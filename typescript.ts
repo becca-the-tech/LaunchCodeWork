cefeda687a495ca9d3578f03707cda3678931b7c
Becca <becca.the.tech@gmail.com>
2020-06-06 11:44:10 -0400

    Added practice for classes and interfaces in Typescript

--- typescript.ts
+++ typescript.ts
@@ -38,3 +38,74 @@ console.log(addUp(1));
 console.log(addUp(1, 9));
 console.log(defaultAdd(1));
 console.log(defaultAdd(1, 9));
+
+class Person {
+    name: string;
+    birthDate: string;
+    constructor(firstName: string, lastName: string, birthYear: number, birthMonth: number, birthDay: number) {
+        this.name = firstName + " " + lastName;
+        this.birthDate = birthMonth + "/" + birthDay + "/" + birthYear;
+    }
+
+    greet() {
+        return "Hello, " + this.name;
+    }
+}
+
+class Child extends Person {
+    parentName1: string;
+    parentName2: string;
+
+    super(name: string, birthDate: string) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    assignParents(mom: string, dad: string) {
+        this.parentName1 = mom;
+        this.parentName2 = dad;
+    }
+
+    announceParents(): string {
+        return `Hi, I'm ${this.name}. My parents are ${this.parentName1} and ${this.parentName2}.
+        And my birth date is: ${this.birthDate}`;
+    }
+}
+
+let Becca = new Person("Becca", "Williams", 1992, 3, 27);
+let Alex = new Person("Alex", "V", 1952, 1, 1)
+let baby = new Child("Skye", "Blue", 2030, 9, 21)
+baby.assignParents("Azure", "Slate");
+console.log(baby.announceParents());
+
+console.log(baby);
+
+interface Book {
+    pageNumber: number;
+}
+
+class Novel implements Book {
+    pageNumber: number;
+
+    constructor() {
+        this.pageNumber = 500;
+    }
+}
+
+class LongStory implements Book {
+    pageNumber: number;
+
+    constructor() {
+        this.pageNumber = 1200;
+    }
+}
+
+function readBook(bookType: Book) {
+    console.log(`Reading a book that has ${bookType.pageNumber} pages!`)
+}
+
+let regularBook = new Novel();
+let longBook = new LongStory();
+
+readBook(regularBook);
+readBook(longBook);
